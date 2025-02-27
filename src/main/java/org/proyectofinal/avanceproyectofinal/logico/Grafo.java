b59f43982e7442083642681d05460a38746ac026
Jeverlin Ramos <jmrc0005@ce.pucmm.edu.do>
2025-02-27 10:13:20 -0400

    Se agregaron los métodos agregar y eliminar paradas y rutas

--- src/main/java/org/proyectofinal/avanceproyectofinal/logico/Grafo.java
+++ src/main/java/org/proyectofinal/avanceproyectofinal/logico/Grafo.java
@@ -20,5 +20,42 @@ public class Grafo {
     public Map<Parada, List<Ruta>> getAdjList() {
         return adjList;
     }
-  
+
+      // Metodos
+
+      // Agregar parada
+      public void agregarParada(Parada p) {
+        paradas.add(p);
+    }
+
+    // Eliminar parada
+    public void eliminarParada(Parada p) {
+        paradas.remove(p);
+        adjList.remove(p);
+        // Eliminar rutas donde el parametro p es destino
+        for (List<Ruta> rutas : adjList.values()) {
+            rutas.removeIf(r -> r.getDestino().equals(p));
+        }
+    }
+
+    // Agregar ruta
+    public void addRoute(Parada origen, Parada destino, double tiempo, double distancia, double costo) {
+        Ruta ruta = new Ruta(origen, destino, tiempo, distancia, costo);
+        adjList.computeIfAbsent(origen, k -> new ArrayList<>()).add(ruta);
+    }
+
+    // Eliminar ruta
+    public void removeRoute(Parada origen, Parada destino) {
+        List<Ruta> rutas = adjList.get(origen);
+        if (rutas != null) {
+            rutas.removeIf(r -> r.getDestino().equals(destino));
+        }
+    }
+
+    // Contabilizar cuantas rutas tiene una parada
+    public int getConnectionCount(Parada p) {
+        List<Ruta> rutas = adjList.get(p);
+        return (rutas != null) ? rutas.size() : 0;
+    }
+
 }
