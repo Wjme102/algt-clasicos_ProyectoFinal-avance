bae0e318f3db8613a415ccfe843ff661cbacb332
Wady <166873861+Wjme102@users.noreply.github.com>
2025-03-10 04:20:57 -0400

    Update GrafoVisual.java
    Estructura base con método main de la visual

--- src/main/java/org/proyectofinal/avanceproyectofinal/visual/GrafoVisual.java
+++ src/main/java/org/proyectofinal/avanceproyectofinal/visual/GrafoVisual.java
@@ -1,4 +1,42 @@
 package org.proyectofinal.avanceproyectofinal.visual;
 
-public class GrafoVisual {
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+import org.proyectofinal.avanceproyectofinal.logico.Grafo;
+import org.proyectofinal.avanceproyectofinal.logico.Parada;
+import org.proyectofinal.avanceproyectofinal.logico.Ruta;
+
+import java.util.*;
+
+public class GrafoVisual extends Application {
+
+    private Grafo grafoLogico;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+       
+    }
 }
