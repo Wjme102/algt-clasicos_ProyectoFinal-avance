96e3b620e0758b321fc942f38d345ee98d317266
Wady <166873861+Wjme102@users.noreply.github.com>
2025-04-09 22:36:08 -0400

    Update Parada.java

--- src/main/java/org/proyectofinal/avanceproyectofinal/logico/Parada.java
+++ src/main/java/org/proyectofinal/avanceproyectofinal/logico/Parada.java
@@ -1,56 +1,38 @@
 package org.proyectofinal.avanceproyectofinal.logico;
 
-import java.util.*;
+import java.util.Objects;
+import java.util.UUID;
+import java.io.Serializable;
 
-public class Parada {
-
+public class Parada implements Serializable {
+    private static final long serialVersionUID = 1L;
     private final UUID id;
-    private double x,y;
+    private double x, y;
     private String nombre;
 
-    // Constructor
-    public Parada(double x, double y, String nombre){
-        this.id=UUID.randomUUID();
+    public Parada(double x, double y, String nombre) {
+        this.id = UUID.randomUUID();
         this.x = x;
         this.y = y;
         this.nombre = nombre;
     }
 
-    // Getters
-    public double getX() {
-        return x;
+    public double getX() { return x; }
+    public double getY() { return y; }
+    public String getNombre() { return nombre; }
+    public UUID getId() {
+        return id;
     }
 
-    public double getY() {
-        return y;
-    }
+    public void setX(double x) { this.x = x; }
+    public void setY(double y) { this.y = y; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    // Setters
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    // Metodo para imprimir el objeto parada en string
     @Override
     public String toString() {
         return (nombre != null && !nombre.isEmpty()) ? nombre : String.format("(%.0f, %.0f)", x, y);
     }
 
-    /* Compara si las paradas son iguales
-    *  Recibe un objeto (en este caso una parada)
-    * */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -59,10 +41,8 @@ public class Parada {
         return id.equals(p.id);
     }
 
-    // Genera un codigo hash unico para cada objeto parada
     @Override
     public int hashCode() {
         return Objects.hash(id);
     }
-
 }
